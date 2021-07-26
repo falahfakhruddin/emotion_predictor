d8956762b5040cf91946db0af163f1a2cb5ca6ff
Naufal Ihsan <nopaleon.bonaparty@gmail.com>
2021-07-26 09:59:52 +0700

    Emotion Color Added

--- app/api/routes.py
+++ app/api/routes.py
@@ -7,10 +7,11 @@ from flask import request, render_template
 @app.route("/",methods=['POST'])
 def predict_emo():
     sentence = [x for x in request.form.values()][0]
-    opening = "your emotion dominated by..."
+    quoted = '"' + sentence + '"'
+    opening = "Your emotion is dominated by..."
     prediction = predictor(sentence)
     index = np.random.randint(0,3)
     reply = response(prediction,index)
     color_config = emotion_color(prediction)
-    return render_template('index.html', sentence_text=sentence, opening_text=opening, prediction_text= prediction, reply_text = reply, color_config = color_config )
+    return render_template('index.html', sentence_text=quoted, opening_text=opening, prediction_text= prediction, reply_text = reply, color_config = color_config )
 
